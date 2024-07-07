2b01e43692340cb962de0baa7495e4de26e75f65
Maksym <tarasovcad@gmail.com>
2024-07-06 21:01:20 -0500

    feat: fetch data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, {ReactNode} from "react";
 import MainText from "./components/MainText";
 import SingleCharacter from "./components/SingleCharacter";
 
-export default class App extends React.Component {
+interface Props {
+  children: ReactNode;
+}
+
+interface Character {
+  id: number;
+  name: string;
+  image: string;
+}
+
+interface Characters {
+  characters: {
+    results: [];
+  };
+}
+
+export default class App extends React.Component<Props, Characters> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      characters: {
+        results: [],
+      },
+    };
+  }
+  componentDidMount(): void {
+    console.log("componentDidMount");
+    fetch("https://rickandmortyapi.com/api/character/?page=1")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({characters: data});
+      });
+  }
   render() {
     return (
       <div className="container">
         <MainText />
         <div className="grid-container">
-          <SingleCharacter />
-          <SingleCharacter />
-          <SingleCharacter />
-          <SingleCharacter />
-          <SingleCharacter />
-          <SingleCharacter />
-          <SingleCharacter />
-          <SingleCharacter />
+          {this.state.characters.results.map((character: Character) => {
+            return <SingleCharacter key={character.id} />;
+          })}
         </div>
       </div>
     );
