2b5996a795287571a1e05185729529fc30cf969e
Maksym <tarasovcad@gmail.com>
2024-07-25 23:27:04 -0500

    feat: display modalMenu only if there is a checked item

--- src/components/ModalMenu.tsx
+++ src/components/ModalMenu.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../store";
 import {unselectAllItems} from "../store/slices/selectedDataSlice";
 import ExportCSV from "../api/downloadAllItems";
+
 const ModalMenu = () => {
+  const [isVisible, setIsVisible] = useState(false);
   const dispatch = useDispatch();
   const selectedItems = useSelector(
     (state: RootState) => state.selectedData.selectedItems,
   );
+  useEffect(() => {
+    setIsVisible(selectedItems.length > 0);
+  }, [selectedItems]);
+
   const unselectAllCheckboxes = () => {
     dispatch(unselectAllItems());
   };
 
   return (
-    <div className="flex sticky bottom-[50px] bg-main p-4 rounded-lg shadow-lg w-fit m-auto items-center">
+    <div
+      className={`flex transition-all sticky bottom-[50px] bg-main p-4 rounded-lg shadow-lg w-fit m-auto items-center ${isVisible ? "translate-y-0" : "translate-y-[1000px]"}`}>
       <h1 className="text-lg font-semibold p-0 m-0">
         {selectedItems.length} items are selected
       </h1>
