1d4e3f463ef3af97d3145cce7c8eb906b20bc891
Maksym <tarasovcad@gmail.com>
2024-08-11 00:47:00 -0500

    fix: fix eslint.config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,34 @@ import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 import { fixupConfigRules } from '@eslint/compat';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node,
+        React: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'react-compiler': { rules: { 'react-compiler': 'error' } },
+    },
+    rules: {
+      ...pluginReactConfig.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,37 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
-import { Character, CharactersData, ParamTypes } from '@/types/types';
-import { useRouter } from 'next/router';
-import Loader from './loader/Loader';
-import SingleCharacter from './SingleCharacter';
-import { FetchDataByTerm } from '@/hooks/useRickAndMortiData';
-import ModalMenu from './redux/ModalMenu';
-import Pagination from './Pagination';
-import DetailedCard from './DetailedCard';
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { Character } from "@/types/types";
+import { useRouter } from "next/router";
+import Loader from "./loader/Loader";
+import SingleCharacter from "./SingleCharacter";
+import { FetchDataByTerm } from "@/hooks/useRickAndMortiData";
+import ModalMenu from "./redux/ModalMenu";
+import Pagination from "./Pagination";
+import DetailedCard from "./DetailedCard";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Main() {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const searchParams = useSearchParams();
   const [detailedcardID, setDetailedcardID] = useState<number>();
-  const { characters, notFound, totalPages, isLoading } = FetchDataByTerm(term, currentPage);
+  const { characters, notFound, totalPages, isLoading } = FetchDataByTerm(
+    term,
+    currentPage,
+  );
   const router = useRouter();
-  
+
   useEffect(() => {
     if (searchParams.size > 0) {
       setCurrentPage(Number(searchParams));
     } else {
       router.push(`/search/${currentPage}`);
     }
-    const items = JSON.parse(localStorage.getItem('tarasovcadLocalStorage') || '{}');
+    const items = JSON.parse(
+      localStorage.getItem("tarasovcadLocalStorage") || "{}",
+    );
     if (items) {
       setTerm(items);
     }
@@ -36,22 +40,26 @@ export default function Main() {
     setDetailedcardID(id);
     router.push({
       pathname: router.pathname,
-      query: { ...router.query, details: '1' },
+      query: { ...router.query, details: "1" },
     });
   }
-  const isDetailsOpen = Boolean(Number(searchParams.get('details'))) || false;
+  const isDetailsOpen = Boolean(Number(searchParams.get("details"))) || false;
   return (
     <main className={` ${inter.className}`}>
       <div className="container">
-        <h1 className="characters mb-[4px] mt-[50px]" data-testid="main-heading">
-          {term ? `Search results for: ${term}` : 'Characters'}
+        <h1
+          className="characters mb-[4px] mt-[50px]"
+          data-testid="main-heading"
+        >
+          {term ? `Search results for: ${term}` : "Characters"}
         </h1>
         <h2 className="description mb-[45px]">
           All of the characters that appear in the
           <a
             href="https://rickandmorty.fandom.com/wiki/Rickipedia"
             target="_blank"
-            rel="noreferrer">
+            rel="noreferrer"
+          >
             <em> Rick and Morty </em>
           </a>
           franchise.
@@ -66,7 +74,7 @@ export default function Main() {
         ) : (
           <>
             <div className="flex mt-10">
-              <div data-testid="grid-container" className={'grid-container'}>
+              <div data-testid="grid-container" className={"grid-container"}>
                 {characters?.results &&
                   characters.results.map((character: Character) => {
                     return (
@@ -92,7 +100,7 @@ export default function Main() {
                   hideDetailedCard={() => {
                     router.push({
                       pathname: router.pathname,
-                      query: { ...router.query, details: '0' },
+                      query: { ...router.query, details: "0" },
                     });
                   }}
                 />
