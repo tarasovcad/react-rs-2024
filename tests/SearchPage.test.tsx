1cc79718a654392eb83710c7a5d42e8123b833ad
Maksym <tarasovcad@gmail.com>
2024-08-13 22:18:43 -0500

    fix: fix SearchPage test

--- tests/SearchPage.test.tsx
+++ tests/SearchPage.test.tsx
@@ -1,33 +1,37 @@
-import { describe, it, expect, vi, beforeEach } from 'vitest';
-import * as cookie from 'cookie';
-import { loader } from '~/routes/search.$page';
-import { AppLoadContext, LoaderFunctionArgs } from '@remix-run/node';
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import * as cookie from "cookie";
+import { loader } from "~/routes/search.$page";
+import { AppLoadContext, LoaderFunctionArgs } from "@remix-run/node";
 
-vi.mock('cookie', () => ({
+vi.mock("cookie", () => ({
   parse: vi.fn(),
 }));
 
-describe('Loader function', () => {
+describe("Loader function", () => {
   beforeEach(() => {
     vi.resetAllMocks();
   });
 
-  it('should return correct data for successful API response', async () => {
-    const mockRequest = new Request('http://localhost:3000/characters/1?details=0');
-    mockRequest.headers.get = vi.fn().mockReturnValue('mockCookie');
+  it("should return correct data for successful API response", async () => {
+    const mockRequest = new Request(
+      "http://localhost:3000/characters/1?details=0"
+    );
+    mockRequest.headers.get = vi.fn().mockReturnValue("mockCookie");
 
-    vi.spyOn(cookie, 'parse').mockReturnValue({ tarasovcadCookieAppRemix: 'testTerm' });
+    vi.spyOn(cookie, "parse").mockReturnValue({
+      tarasovcadCookieAppRemix: "testTerm",
+    });
 
     global.fetch = vi.fn().mockResolvedValue({
       json: vi.fn().mockResolvedValue({
-        results: [{ id: 1, name: 'Rick' }],
+        results: [{ id: 1, name: "Rick" }],
         info: { pages: 5 },
       }),
     });
 
     const mockLoaderArgs: LoaderFunctionArgs = {
       request: mockRequest,
-      params: { page: '1' },
+      params: { page: "1" },
       context: {} as AppLoadContext,
     };
 
@@ -36,29 +40,31 @@ describe('Loader function', () => {
 
     expect(data).toEqual({
       currentPage: 1,
-      details: '0',
+      details: "0",
       characters: {
-        results: [{ id: 1, name: 'Rick' }],
+        results: [{ id: 1, name: "Rick" }],
         info: { pages: 5 },
       },
       notFound: false,
       totalPages: 5,
-      term: 'testTerm',
+      term: "testTerm",
       isLoading: false,
       detailedcardID: 0,
     });
   });
 
-  it('should handle API error correctly', async () => {
-    const mockRequest = new Request('http://localhost:3000/characters/1');
-    mockRequest.headers.get = vi.fn().mockReturnValue('mockCookie');
+  it("should handle API error correctly", async () => {
+    const mockRequest = new Request("http://localhost:3000/characters/1");
+    mockRequest.headers.get = vi.fn().mockReturnValue("mockCookie");
 
-    vi.spyOn(cookie, 'parse').mockReturnValue({ tarasovcadCookieAppRemix: 'testTerm' });
+    vi.spyOn(cookie, "parse").mockReturnValue({
+      tarasovcadCookieAppRemix: "testTerm",
+    });
 
-    global.fetch = vi.fn().mockRejectedValue(new Error('API Error'));
+    global.fetch = vi.fn().mockRejectedValue(new Error("API Error"));
     const mockLoaderArgs: LoaderFunctionArgs = {
       request: mockRequest,
-      params: { page: '1' },
+      params: { page: "1" },
       context: {} as AppLoadContext,
     };
 
@@ -67,38 +73,40 @@ describe('Loader function', () => {
 
     expect(data).toEqual({
       currentPage: 1,
-      details: '0',
+      details: "0",
       characters: null,
       notFound: true,
       totalPages: 0,
-      term: 'testTerm',
+      term: "testTerm",
       isLoading: false,
       detailedcardID: undefined,
     });
   });
 
-  it('should handle missing page parameter', async () => {
-    const mockRequest = new Request('http://localhost:3000/characters');
-    mockRequest.headers.get = vi.fn().mockReturnValue('mockCookie');
+  it("should handle missing page parameter", async () => {
+    const mockRequest = new Request("http://localhost:3000/characters");
+    mockRequest.headers.get = vi.fn().mockReturnValue("mockCookie");
 
-    vi.spyOn(cookie, 'parse').mockReturnValue({ tarasovcadCookieAppRemix: 'testTerm' });
+    vi.spyOn(cookie, "parse").mockReturnValue({
+      tarasovcadCookieAppRemix: "testTerm",
+    });
 
     global.fetch = vi.fn().mockResolvedValue({
       json: vi.fn().mockResolvedValue({
-        results: [{ id: 1, name: 'Rick' }],
+        results: [{ id: 1, name: "Rick" }],
         info: { pages: 5 },
       }),
     });
 
     const mockLoaderArgs: LoaderFunctionArgs = {
       request: mockRequest,
-      params: { page: '1' },
+      params: { page: "1" },
       context: {} as AppLoadContext,
     };
 
     const result = (await loader(mockLoaderArgs)) as Response;
     const data = await result?.json();
 
-    expect(data.currentPage).toBe('1');
+    expect(data.currentPage).toBe(1);
   });
 });
